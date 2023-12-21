c644f46f30c0f333ae01dfef8cb033ee0874909a
Mikhail-Iv-638 <152494296+Mikhail-Iv-638@users.noreply.github.com>
2023-12-21 14:33:36 +0300

    Update solar_read.py

--- Центробежное отделение/solar_read.py
+++ Центробежное отделение/solar_read.py
@@ -92,25 +92,6 @@ def parse_planet_parameters(line, planet):
     planet.Vy = float(tokens[7])
 
 
-def write_space_objects_data_to_file(output_filename, space_objects):
-    """Сохраняет данные о космических объектах в файл.
-
-    Строки должны иметь следующий формат:
-
-    Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
-    Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
-    Параметры:
-
-    **output_filename** — имя входного файла
-
-    **space_objects** — список объектов планет и звёзд
-    """
-    with open(output_filename, 'w') as out_file:
-        for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            
 
 
 if __name__ == "__main__":
