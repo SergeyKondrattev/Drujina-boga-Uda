650a75d70e3eb3fad1f7652295d9d1790f8269a0
Mikhail-Iv-638 <152494296+Mikhail-Iv-638@users.noreply.github.com>
2023-12-21 14:28:33 +0300

    Update solar_physics.py

--- столкновение/solar_physics.py
+++ столкновение/solar_physics.py
@@ -1,5 +1,10 @@
+from random import randint
+from solar_visuals import *
+
 gravitational_constant = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
+
+
 
 class Star():
     """Тип данных, описывающий звезду.
@@ -71,7 +76,7 @@ class Planet():
     Fy = 0
     """Сила по оси **y**"""
 
-    R = 5
+    R = 0
     """Радиус планеты"""
 
     color = "green"
@@ -81,19 +86,7 @@ class Planet():
     """Изображение планеты"""
 
 
-class Fragment(Planet):
-    """Тип данных, описывающий осколки от столкновения планет.
-    Содержит массу, координаты, скорость планеты,
-    а также визуальный радиус планеты в пикселах и её цвет
-    """
-    type = "fragment"
-
-    R = 1
-
-    color = "purple"
-    
-    
-def calculate_force(body, space_objects):
+def calculate_force(body, space_objects, W):
     """Вычисляет силу, действующую на тело.
 
     Параметры:
@@ -103,33 +96,86 @@ def calculate_force(body, space_objects):
     **space_objects** — список объектов, которые воздействуют на тело.
     """
 
+    sf = 2.4e-08
     body.Fx = body.Fy = 0
     for obj in space_objects:
-        if body == obj:
+        if ((body.x - obj.x)**2 + (body.y - obj.y)**2) == 0:
             continue
+        if ((sf * body.x - sf * obj.x)**2 + (sf * body.y - sf * obj.y)**2)**0.5 > 0.95*(body.R + obj.R) and((sf * body.x - sf * obj.x)**2 + (sf * body.y - sf * obj.y)**2)**0.5 <= (body.R + obj.R) and body.m != 0:
+            center_m = body.m + obj.m
+            center_x = (body.x * body.m + obj.x * obj.m)/center_m
+            center_y = (body.y * body.m + obj.y * obj.m)/center_m
+            rc_x = body.x - center_x
+            rc_y = body.y - center_y
+            body.Fx += body.m * W**2 * rc_x
+            body.Fy += body.m * W**2 * rc_y
+            """Вычисляет центробежную силу."""
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
         body.Fx += gravitational_constant * body.m * obj.m / r**3 * (obj.x - body.x)
         body.Fy += gravitational_constant * body.m * obj.m / r**3 * (obj.y - body.y)
 
 
-def move_space_object(body, dt):
+def move_space_object(body, dt, space_objects, W):
     """Перемещает тело в соответствии с действующей на него силой.
 
     Параметры:
 
     **body** — тело, которое нужно переместить.
     """
+    sf = 2.4e-08
+    if body.m != 0:
+        ax = body.Fx / body.m
+        body.x += body.Vx * dt + ax * dt**2 / 2
+        body.Vx += ax * dt
+        ay = body.Fy / body.m
+        body.y += body.Vy * dt + ay * dt**2 / 2
+        body.Vy += ay * dt
+    """Проверяет слипшиеся тела и вращает их с заданной угловой скоростью"""
+    for obj in space_objects:
+        if obj == body or obj.m == 0:
+            continue
+        elif ((sf * body.x - sf * obj.x)**2 + (sf * body.y - sf * obj.y)**2)**0.5 > (body.R + obj.R):
+            continue
+        elif ((sf * body.x - sf * obj.x)**2 + (sf * body.y - sf * obj.y)**2)**0.5 < 0.95*(body.R + obj.R):
+            x1 = body.x - obj.x
+            y1 = body.y - obj.y
+            if (x1 != 0) and (y1 != 0):
+                body.x = obj.x + x1 / abs(x1) * (body.R+obj.R)/(1 + x1**2/y1**2)**0.5/(sf)
+                body.y = obj.y + y1 / abs(y1) * (body.R+obj.R)/(1 + x1**2/y1**2)**0.5/(sf)
+            elif (x1 != 0):
+                body.x = obj.x + x1 / abs(x1) * (body.R+obj.R)/(sf)
+                body.y = obj.y
+            else:
+                body.y = obj.y + y1 / abs(y1) * (body.R+obj.R)/(sf)
+                body.x = obj.x
+        elif obj.m != 0:
+            center_m = body.m + obj.m
+            center_x = (body.x * body.m + obj.x * obj.m)/center_m
+            center_y = (body.y * body.m + obj.y * obj.m)/center_m
 
-    ax = body.Fx / body.m
-    body.Vx += ax * dt
-    body.x += body.Vx * dt
-    ay = body.Fy / body.m
-    body.Vy += ay * dt
-    body.y += body.Vy * dt
+            rc_x = body.x - center_x
+            rc_y = body.y - center_y
+            r = (rc_y**2 + rc_x**2)**0.5
+            sin = rc_y/r
+            cos = rc_x/r
+            v = W * r
+            if sin * cos >= 0:
+                sin = -rc_y/r
+                cos = rc_x/r
+            elif sin * cos < 0:
+                sin = rc_y/r
+                cos = -rc_x/r
+            body.Vx = v * cos
+            body.Vy = v * sin
+
+            body.x += body.Vx * dt
+            body.y += body.Vy * dt
+
+
     '''zhitь ( ͡ಥ ͜ʖ ͡ಥ)'''
 
 
-def recalculate_space_objects_positions(space_objects, dt):
+def recalculate_space_objects_positions(space_objects, dt, W):
     """Пересчитывает координаты объектов.
 
     Параметры:
@@ -140,10 +186,10 @@ def recalculate_space_objects_positions(space_objects, dt):
     """
 
     for body in space_objects:
-        calculate_force(body, space_objects)
+        calculate_force(body, space_objects, W)
     for body in space_objects:
-        move_space_object(body, dt)
-  
+        move_space_object(body, dt, space_objects, W)
+
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
